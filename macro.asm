f119ddc19f52b7e9725bba38bac155244f70b4d2
Sebastián Orozco Castillo <159093795+CastilloSebas2005@users.noreply.github.com>
2024-11-02 03:43:04 -0600

    Haciendo calcular generico con macro

--- macro.asm
+++ macro.asm
@@ -1,4 +1,4 @@
-operacion macro op1, op2, operador
+operacion macro ope1, ope2, operador
     cmp operador, '+'
     je sumar
     cmp operador, '-'
@@ -10,31 +10,48 @@ operacion macro op1, op2, operador
     jmp fin
 
 sumar:
-    mov al, op1
-    add al, op2
-    mov op1, al
+    mov al, ope1
+    add al, ope2
+    mov ope1, al
     jmp fin
 
 restar:
-    mov al, op1
-    sub al, op2
-    mov op1, al
+    mov al, ope1
+    sub al, ope2
+    mov ope1, al
     jmp fin
 
 dividir:
-    mov al, op1
+    mov al, ope1
     cbw
-    mov bl, op2
+    mov bl, ope2
     div bl
-    mov op1, al
+    mov ope1, al
     jmp fin
 
 multiplicar:
-    mov al, op1
-    mov bl, op2
+    mov al, ope1
+    mov bl, ope2
     imul bl
-    mov op1, al
+    mov ope1, al
     jmp fin
 
 fin:
+endm
+
+calcularM macro vector, op1, op2, operador
+    mov cx, 25
+    mov si, 1
+    mov al, 0 ;va a almacenar el resultado final
+    hacerCalculo:
+    mov al, [vector]
+    mov op1, al
+    mov al, [vector + si + 1]
+    mov op2, al
+    mov al, [vector + si]
+    mov operador, al
+    operacion op1, op2, operador
+    mov [vector], al
+    add si, 2
+    loop hacerCalculo
 endm
